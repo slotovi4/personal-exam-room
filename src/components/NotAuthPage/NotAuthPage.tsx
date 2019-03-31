852079eeb1f94fb0b332d8b6b8ea0febbc7220a5
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-03-31 19:49:35 +0300

    added NotAuthPage styles

--- src/components/NotAuthPage/NotAuthPage.tsx
+++ src/components/NotAuthPage/NotAuthPage.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Redirect } from 'react-router-dom';
+import { cn } from '@bem-react/classname';
+import './NotAuthPage.scss';
 
 interface IProps {
   loggingIn: boolean;
@@ -8,8 +10,12 @@ interface IProps {
 export default (ComposedComponent: any) =>
   class NotAuthPage extends React.Component<IProps> {
     public render() {
+      const page = cn('NotAuthPage');
+
       return !this.props.loggingIn ? (
-        <ComposedComponent {...this.props} />
+        <div className={page()}>
+          <ComposedComponent {...this.props} />
+        </div>
       ) : (
         <Redirect to={'/'} />
       );
