79df585370dd38bc59a858d9214fb3a2a03e932e
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-03-31 16:48:41 +0300

    rename UserIsNotAuthenticated component to NotAuthPage component

--- src/containers/notAuthContainer.ts
+++ src/containers/notAuthContainer.ts
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
-import UserIsNotAuthenticated from '../components/UserIsNotAuthenticated/UserIsNotAuthenticated';
+import NotAuthPage from '../components/NotAuthPage/NotAuthPage';
 import { LoginContainer } from '../containers/loginContainer';
 
 export const NotAuthLoginContainer = connect((state: any) => ({
   loggingIn: state.login.loggingIn
-}))(UserIsNotAuthenticated(LoginContainer));
+}))(NotAuthPage(LoginContainer));
