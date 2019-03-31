0e3ce01e6b76682669ca0c869f9099d31e0bf223
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-03-31 14:45:04 +0300

    update Menu component

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,18 +4,31 @@ import { Link } from 'react-router-dom';
 interface IProps {
   firstName: string;
   lastName: string;
+  getUserProfile: () => void;
 }
 
-const Menu = ({ firstName, lastName }: IProps) => {
-  return (
-    <section>
-      <h5>
-        {firstName} {lastName}
-      </h5>
-      <Link to="/profile">профилль</Link>
-      <Link to="/exams">расписание экзаменов</Link>
-    </section>
-  );
-};
+class Menu extends React.Component<IProps> {
+  public componentWillMount() {
+    const { lastName, firstName } = this.props;
+
+    if (!lastName && !firstName) {
+      this.props.getUserProfile();
+    }
+  }
+
+  public render() {
+    const { firstName, lastName } = this.props;
+
+    return (
+      <section>
+        <h5>
+          {firstName} {lastName}
+        </h5>
+        <Link to="/profile">профилль</Link>
+        <Link to="/exams">расписание экзаменов</Link>
+      </section>
+    );
+  }
+}
 
 export default Menu;
--- src/containers/menuContainer.ts
+++ src/containers/menuContainer.ts
@@ -1,7 +1,11 @@
 import { connect } from 'react-redux';
+import { getUserProfile } from '../actions/profileActions';
 import Menu from '../components/Menu/Menu';
 
-export const MenuContainer = connect((state: any) => ({
-  firstName: state.profile.firstName,
-  lastName: state.profile.lastName
-}))(Menu);
+export const MenuContainer = connect(
+  (state: any) => ({
+    firstName: state.profile.firstName,
+    lastName: state.profile.lastName
+  }),
+  { getUserProfile }
+)(Menu);
