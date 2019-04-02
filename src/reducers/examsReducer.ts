11262e9fc0c52191985615c54626f1a85dc1cfea
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-04-02 21:09:54 +0300

    added examsReducer.test

--- src/reducers/examsReducer.ts
+++ src/reducers/examsReducer.ts
@@ -1,14 +1,14 @@
 import { examsTypes } from '../actions/types';
 import { IExams } from '../actions/interface';
 
-interface IAction {
-  type: 'GET_EXAMS';
+export interface IAction {
+  type?: 'GET_EXAMS';
   exams?: IExams;
 }
 
-const { GET_EXAMS } = examsTypes;
+export const initialState: IExams | {} = {};
 
-const initialState: IExams | {} = {};
+const { GET_EXAMS } = examsTypes;
 
 export default (state = initialState, action: IAction) => {
   switch (action.type) {
---
+++ src/reducers/test/examsReducer.test.ts
@@ -0,0 +1,39 @@
+import Exam, { IAction, initialState } from '../examsReducer';
+import { IExams } from '../../actions/interface';
+import * as deepFreeze from 'deep-freeze';
+
+describe('Exams reducer', () => {
+  it('GET_EXAMS sucess', () => {
+    const state: IExams | {} = {};
+    const exam = {
+      id: 'f2762d48-5045-43a9-bf05-d8728771d820',
+      date: '2019-04-02T16:50:00+00:00',
+      qualificationTitle: 'Администрирование баз данных',
+      qualificationLevel: '6',
+      occupationalStandardTitle: 'occupation 1',
+      examinationCenterName: 'Первый ЭЦ',
+      address: 'ул. Петровка 38',
+      city: 'Москва',
+      status: 'isApplying',
+      statusChangeTime: '0001-01-01T00:00:00+00:00',
+      duration: 90
+    };
+    const action: IAction = {
+      type: 'GET_EXAMS',
+      exams: { exams: [exam], currentPage: 1, pageSize: 10, total: 11 }
+    };
+
+    deepFreeze([state, action]);
+    const result = Exam(state, action);
+
+    expect(result).toEqual({
+      exams: [exam],
+      currentPage: 1,
+      pageSize: 10,
+      total: 11
+    });
+  });
+
+  it('Defaults array for incorrect action', () =>
+    expect(Exam(initialState, {})).toEqual({}));
+});
