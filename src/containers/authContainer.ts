7c52426fc5fa27ea03fd36bff52e856daf6825f1
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-04-02 18:35:31 +0300

    update authContainer

--- src/containers/authContainer.ts
+++ src/containers/authContainer.ts
@@ -4,17 +4,12 @@ import { ExamsContainer } from '../containers/examsContainer';
 import AuthPage from '../components/AuthPage/AuthPage';
 import Home from '../components/Home/Home';
 
-export const AuthHomeContainer = connect((state: any) => ({
-  loggingIn: state.login.loggingIn,
-  theme: state.theme.theme
-}))(AuthPage(Home));
+const AuthContainer = (component: any) =>
+  connect((state: any) => ({
+    loggingIn: state.login.loggingIn,
+    theme: state.theme.theme
+  }))(AuthPage(component));
 
-export const AuthProfileContainer = connect((state: any) => ({
-  loggingIn: state.login.loggingIn,
-  theme: state.theme.theme
-}))(AuthPage(ProfileContainer));
-
-export const AuthExamsContainer = connect((state: any) => ({
-  loggingIn: state.login.loggingIn,
-  theme: state.theme.theme
-}))(AuthPage(ExamsContainer));
+export const AuthHomeContainer = AuthContainer(Home);
+export const AuthProfileContainer = AuthContainer(ProfileContainer);
+export const AuthExamsContainer = AuthContainer(ExamsContainer);
