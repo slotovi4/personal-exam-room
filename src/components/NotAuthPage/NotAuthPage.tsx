52d5339e0f0512dc8039093a45f91d1acfe3e077
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-04-02 19:01:45 +0300

    added types to NotAuthPage

--- src/components/NotAuthPage/NotAuthPage.tsx
+++ src/components/NotAuthPage/NotAuthPage.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import { Redirect } from 'react-router-dom';
 import { cn } from '@bem-react/classname';
+import { ConnectedComponentClass } from 'react-redux';
 import './NotAuthPage.scss';
 
 interface IProps {
   loggingIn: boolean;
 }
 
-export default (ComposedComponent: any) =>
+export default (
+  ComposedComponent:
+    | ConnectedComponentClass<any, any>
+    | React.ClassicComponentClass
+    | React.FunctionComponent
+) =>
   class NotAuthPage extends React.Component<IProps> {
     public render() {
       const page = cn('NotAuthPage');
--- src/containers/notAuthContainer.ts
+++ src/containers/notAuthContainer.ts
@@ -1,7 +1,15 @@
-import { connect } from 'react-redux';
-import NotAuthPage from '../components/NotAuthPage/NotAuthPage';
+import { connect, ConnectedComponentClass } from 'react-redux';
 import { LoginContainer } from '../containers/loginContainer';
+import NotAuthPage from '../components/NotAuthPage/NotAuthPage';
 
-export const NotAuthLoginContainer = connect((state: any) => ({
-  loggingIn: state.login.loggingIn
-}))(NotAuthPage(LoginContainer));
+const NotAuthContainer = (
+  component:
+    | ConnectedComponentClass<any, any>
+    | React.ClassicComponentClass
+    | React.FunctionComponent
+) =>
+  connect((state: any) => ({
+    loggingIn: state.login.loggingIn
+  }))(NotAuthPage(component));
+
+export const NotAuthLoginContainer = NotAuthContainer(LoginContainer);
