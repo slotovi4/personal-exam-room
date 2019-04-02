112ab574e99a3cafcf9026b3d96f61140f7355e8
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-04-02 15:19:09 +0300

    added Theme component

---
+++ src/components/Theme/Theme.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Theme extends React.Component {
+  public render() {
+    return (
+      <section>
+        <span>Выбор темы</span>
+      </section>
+    );
+  }
+}
+
+export default Theme;
