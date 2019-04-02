2127480fdb5892f6e84b150ff694820d087646d8
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-04-02 16:17:32 +0300

    added Home styles

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { ThemeContainer } from '../../containers/themeContainer';
+import { cn } from '@bem-react/classname';
+import './Home.scss';
 
 const Home = () => {
+  const home = cn('Home');
+
   return (
-    <div>
-      <span>home</span>
+    <section className={home()}>
+      <h1 className={home('Title')}>Главная страница</h1>
       <ThemeContainer />
-    </div>
+    </section>
   );
 };
 
