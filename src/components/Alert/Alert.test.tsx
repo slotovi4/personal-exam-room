1fbef2c552d3d11b8dedb43d45698a0decbd8846
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-04-03 23:59:05 +0300

    update Alert.test

--- src/components/Alert/Alert.test.tsx
+++ src/components/Alert/Alert.test.tsx
@@ -3,19 +3,20 @@ import { shallow } from 'enzyme';
 import { IAlert } from '../../actions/interface';
 import Alert from './Alert';
 
+const errorData: IAlert = { messageType: 'error', message: 'error message' };
+const alertData: IAlert = { messageType: 'alert', message: 'alert message' };
+
 describe('<Alert /> Component', () => {
   describe('On error', () => {
     it('Error message are showing', () => {
-      const data: IAlert = { messageType: 'error', message: 'error message' };
-      const alert = shallow(<Alert alert={data} />);
+      const alert = shallow(<Alert alert={errorData} />);
       alert.setState({ show: true });
 
       expect(alert.text()).toEqual('error message');
     });
 
     it('Error styles apply', () => {
-      const data: IAlert = { messageType: 'error', message: 'error message' };
-      const alert = shallow(<Alert alert={data} />);
+      const alert = shallow(<Alert alert={errorData} />);
       alert.setState({ show: true });
 
       expect(alert.find('article').hasClass('Alert_type_error')).toEqual(true);
@@ -24,16 +25,14 @@ describe('<Alert /> Component', () => {
 
   describe('On alert', () => {
     it('Alert message are showing', () => {
-      const data: IAlert = { messageType: 'alert', message: 'alert message' };
-      const alert = shallow(<Alert alert={data} />);
+      const alert = shallow(<Alert alert={alertData} />);
       alert.setState({ show: true });
 
       expect(alert.text()).toEqual('alert message');
     });
 
     it('Alert styles apply', () => {
-      const data: IAlert = { messageType: 'alert', message: 'alert message' };
-      const alert = shallow(<Alert alert={data} />);
+      const alert = shallow(<Alert alert={alertData} />);
       alert.setState({ show: true });
 
       expect(alert.find('article').hasClass('Alert_type_alert')).toEqual(true);
@@ -42,8 +41,7 @@ describe('<Alert /> Component', () => {
 
   describe('On click', () => {
     it('Component hided on click', () => {
-      const data: IAlert = { messageType: 'alert', message: 'alert message' };
-      const alert = shallow(<Alert alert={data} />);
+      const alert = shallow(<Alert alert={alertData} />);
       alert.setState({ show: true });
       alert.simulate('click');
 
