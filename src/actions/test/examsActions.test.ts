6114c0baedf715d7c5045a453c2eb37a61738fc2
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-04-03 20:55:31 +0300

    added examsActions.test

---
+++ src/actions/test/examsActions.test.ts
@@ -0,0 +1,13 @@
+import store from '../../store';
+import { getUserExams } from '../examsActions';
+
+describe('Exams actions', () => {
+  describe('getUserExams', () => {
+    it('should work get exams', () => {
+      const res = getUserExams();
+      res(store.dispatch);
+
+      expect(store.getState().exams).toBeDefined();
+    });
+  });
+});
